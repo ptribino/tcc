add6a3093b527f9fb2559a7af3a32cb73a674a4a
pripessoal <ptribino@gmail.com>
2018-11-07 22:14:08 -0200

    Alteracao arquivo wsgi.py

--- smartlook/wsgi.py
+++ smartlook/wsgi.py
@@ -10,8 +10,13 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+#from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smartlook.settings")
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smartlook.settings")
+
+#application = Cling(get_wsgi_application())
