f3f686be49e9ab20b5613f8a8cd6d82f129a6452
pripessoal <ptribino@gmail.com>
2018-11-08 03:25:53 -0200

    Heroku

--- smartlook/wsgi.py
+++ smartlook/wsgi.py
@@ -10,13 +10,8 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-#from dj_static import Cling
+from dj_static import Cling
 
-from whitenoise.django import DjangoWhiteNoise
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smartlook.settings")
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smartlook.settings")
-
-#application = Cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
