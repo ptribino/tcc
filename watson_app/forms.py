947ff2ecaaeb354ca5251c55b6b9fb34a6e7d7ec
pripessoal <ptribino@gmail.com>
2018-11-15 14:39:15 -0200

    Ajuste erro deploy

--- watson_app/forms.py
+++ watson_app/forms.py
@@ -1,25 +1,19 @@
 from django import forms
-
-
 # Watson dependencies
 import json
 from watson_developer_cloud import AssistantV1
 
 # Getting APIKEY variable from settings
 #APIKEY = getattr(settings, "APIKEY", None)
-
-
 assistant = AssistantV1(
      version='2018-09-20',
      username='d662d4ba-9e5d-4631-b94b-23446011f580',
      password='HlfulHf7UuJQ',
      url='https://gateway.watsonplatform.net/conversation/api/v1/workspaces/d44da73a-4abb-4cd0-9ab1-803488fdec1f/message'
  )
-
 # class CommentForm(forms.Form):
 #     comment = forms.CharField(
 #         label="Comment", widget=forms.Textarea(attrs={'rows': 10}), required=True)
-
      # def ask_watson(self):
      #     text = self.cleaned_data['comment']
      #     combined_operations = ['doc-sentiment']
